df17b45cbd08228f3afb44c0512c681b1e88e3fa
telesyk <telesyk89@gmail.com>
2023-03-10 16:49:13 +0200

    Major template update. Fix issues with boostrap modularity import Setup basic bootstrap styles

--- src/js/test-es6.js
+++ src/js/test-es6.js
@@ -1,7 +1,20 @@
 const arrowFunc = () => {
-    console.log("ES6 func is loaded");
-    const doMath = window.innerWidth - window.innerHeight;
-    console.log("window.innerWidth - window.innerHeight", doMath);
+  const parentElement = document.querySelector('.starter-template');
+
+  const newElement = () => {
+    const template = `
+<pre class="test-class my-4 p-2 border rounded-1">
+<code>Hello there</code>
+</pre>
+    `;
+    const doc = new DOMParser().parseFromString(template, 'text/html');
+    return doc.body.firstChild;
+  };
+
+  if (parentElement) {
+    const testEl = newElement();
+    parentElement.append(testEl);
+  }
 };
 
 export default arrowFunc;
